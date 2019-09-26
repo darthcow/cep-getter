620632493ac39badc9f1816868ceeefbc5f47f7b
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-09-26 10:59:09 -0300

    Improved UI Call is made when cep is inputed, no need to click any button method to hide keyboard added

--- CepGetter/app/src/main/java/com/project/cepgetter/CepService.kt
+++ CepGetter/app/src/main/java/com/project/cepgetter/CepService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CepService {
+    //annotation specifies http request type and URL
     @GET("{cep}/json")
     fun getCep(@Path("cep") cep: String): Call<AddressResponse>
 
--- CepGetter/app/src/main/java/com/project/cepgetter/MainActivity.kt
+++ CepGetter/app/src/main/java/com/project/cepgetter/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.project.cepgetter
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,18 +19,56 @@ class MainActivity : AppCompatActivity() {
 
 //    private lateinit var viewModel: CepViewModel
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        viewModel = ViewModelProviders.of(this).get(CepViewModel::class.java)
 
-        button.setOnClickListener { getCep() }
     }
 
+    //TODO change this code to kotlin extension later
+    fun hideKeyboard(activity: Activity) {
+        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        view.clearFocus()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
-    private fun getCep() {
-        val call = WebClient().cepService().getCep(editText.text.toString())
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                //improvised maskText
+//                if (s.length == 5){
+//                    s.insert(5,"-")
+//                }
+                if (s.length == 8) {
+                    getCep(s.toString())
+                    hideKeyboard(this@MainActivity)
+
+                }
+            }
+        })
+
+    }
+
+    private fun getCep(cep: String) {
+        val call = WebClient().cepService().getCep(cep)
         call.enqueue(object : Callback<AddressResponse?> {
             override fun onResponse(
                 call: Call<AddressResponse?>,
@@ -32,7 +76,8 @@ class MainActivity : AppCompatActivity() {
             ) {
                 response.body().let {
                     textView.text = //it?.logradouro + ", " + it?.localidade + ", " + it?.uf
-                        "${it?.logradouro}, ${it?.localidade}, ${it?.uf}"
+                        "Logradouro: ${it?.logradouro}\nCidade: ${it?.localidade}\nEstado: ${it?.uf}"
+
                 }
             }
 
