3258325053b5575d93b7504e88b6bc37a7f5b67e
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-22 13:05:04 -0300

    Created kotlin custom extensions for toast and hideKeyboard fun

---
+++ CepGetter/app/src/main/java/com/project/cepgetter/extensions/Activity.kt
@@ -0,0 +1,26 @@
+package com.project.cepgetter.extensions
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.shortToast(message: String) =
+    runOnUiThread { Toast.makeText(this, message,  Toast.LENGTH_SHORT).show() }
+
+fun AppCompatActivity.longToast(message: String) =
+    runOnUiThread { Toast.makeText(this, message, Toast.LENGTH_LONG).show() }
+
+
+fun AppCompatActivity.hideKeyboard() {
+    val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    //Find the currently focused view, so we can grab the correct window token from it.
+    var view = this.currentFocus
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    if (view == null) {
+        view = View(this)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+    view.clearFocus()
+}
