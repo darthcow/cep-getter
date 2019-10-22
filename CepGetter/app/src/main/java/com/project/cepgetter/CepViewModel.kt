948842d2673b413452ce5d387e114ce83989140b
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-22 13:41:14 -0300

    Implemented ViewModel architecture

--- CepGetter/app/src/main/java/com/project/cepgetter/CepViewModel.kt
+++ CepGetter/app/src/main/java/com/project/cepgetter/CepViewModel.kt
@@ -1,10 +1,45 @@
 package com.project.cepgetter
 
+import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.project.cepgetter.web.AddressResponse
+import com.project.cepgetter.web.WebClient
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class CepViewModel : ViewModel(){
+class CepViewModel : ViewModel() {
+
+    val address = MutableLiveData<String>()
+    var errorMessage: String? = null
 
 
+    fun getCep(cep: String): String? {
+        val call = WebClient().cepService().getCep(cep)
+        call.enqueue(object : Callback<AddressResponse?> {
+            override fun onResponse(call: Call<AddressResponse?>, response: Response<AddressResponse?>) {
+                response.body()?.let {
+                    address.value =
+                        "Cep: ${it.cep}\nLogradouro: ${it.logradouro}" +
+                                "\nBairro: ${it.bairro}\nCidade: ${it.localidade}\nEstado: ${it.uf}"
+                }
+            }
+
+            override fun onFailure(call: Call<AddressResponse?>, t: Throwable?) {
+                errorMessage = t?.message.toString()
+            }
+        })
+        return errorMessage
+    }
 
 
 }
--- CepGetter/app/src/main/java/com/project/cepgetter/MainActivity.kt
+++ CepGetter/app/src/main/java/com/project/cepgetter/MainActivity.kt
@@ -1,64 +1,43 @@
 package com.project.cepgetter
 
-import android.app.Activity
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.project.cepgetter.web.AddressResponse
-import com.project.cepgetter.web.WebClient
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.project.cepgetter.extensions.hideKeyboard
+import com.project.cepgetter.extensions.longToast
+import com.project.cepgetter.extensions.onTextChange
+import com.project.cepgetter.extensions.shortToast
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var viewModel: CepViewModel
+    private val viewModel by lazy { ViewModelProviders.of(this).get(CepViewModel::class.java) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        viewModel = ViewModelProviders.of(this).get(CepViewModel::class.java)
+        //observe viewmodel's variables and what to do with value
+        viewModel.address.observe(this, Observer<String> { result_address.text = it })
 
     }
 
-
-    //TODO change this code to kotlin extension later
-    fun hideKeyboard(activity: Activity) {
-        val imm = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        //Find the currently focused view, so we can grab the correct window token from it.
-        var view = activity.currentFocus
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = View(activity)
-        }
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-        view.clearFocus()
-    }
 
     private fun copyAddress() {
         try {
             val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("address", result_address.text.toString())
+            val clip = ClipData.newPlainText("address", viewModel.address.value)
             clipboard.setPrimaryClip(clip)
-            //TODO change toast code to kotlin extension later
-            Toast.makeText(this@MainActivity, "Endreço copiado!", Toast.LENGTH_SHORT).show()
-
+            this.shortToast("Endreço copiado!")
         } catch (e: Exception) {
-            Toast.makeText(
-                this,
-                "Erro ao copiar endereço: ${e.localizedMessage}",
-                Toast.LENGTH_LONG
-            ).show()
+            this.longToast("Erro ao copiar endereço: ${e.localizedMessage}")
             println(e.localizedMessage)
         }
     }
@@ -66,47 +45,21 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        //set copyAddress fun to click listener
         result_address.setOnClickListener { copyAddress() }
-        cep_field.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
-            }
+        //called extension fun onTextChange which implements TextWatcher
+        cep_field.onTextChange {
+            //checks if field has required number of characters to make request
+            if (it.length == 8) {
+                //Only displays toast if fun returns a message different than null
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable) {
-                //TODO improvised maskText
-//                if (s.length == 5){
-//                    s.insert(5,"-")
-//                }
-                if (s.length == 8) {
-                    getCep(s.toString())
-                    hideKeyboard(this@MainActivity)
-
+                viewModel.getCep(it)?.let {
+                    this@MainActivity.longToast(it)
                 }
+                this@MainActivity.hideKeyboard()
             }
-        })
-
-    }
-
-    private fun getCep(cep: String) {
-        val call = WebClient().cepService().getCep(cep)
-        call.enqueue(object : Callback<AddressResponse?> {
-            override fun onResponse(call: Call<AddressResponse?>, response: Response<AddressResponse?>) {
-                response.body().let {
-                    result_address.text = //it?.logradouro + ", " + it?.localidade + ", " + it?.uf
-                        "Cep: ${it?.cep}\nLogradouro: ${it?.logradouro}\nBairro: ${it?.bairro}\nCidade: ${it?.localidade}\nEstado: ${it?.uf}"
-
-                }
-            }
-
-
-            override fun onFailure(call: Call<AddressResponse?>, t: Throwable?) {
-                Toast.makeText(this@MainActivity, t?.message, Toast.LENGTH_SHORT).show()
-            }
-        })
+        }
     }
 
 }
